615cd9fc1e259f16b299a1af06cb0b8cd829e732
稲井まりえ <inai_marie@kakaku.com>
2016-01-26 09:47:50 +0900

    add 2 cat voices

--- app/src/main/java/com/cat/morning/goodmorningcat/CatProfileActivity.java
+++ app/src/main/java/com/cat/morning/goodmorningcat/CatProfileActivity.java
@@ -15,6 +15,8 @@ public class CatProfileActivity extends AppCompatActivity {
 
     private SoundPool mSoundPool;
     private int mSoundId;
+    private int mSoundIdKittin;
+    private int mSoundIdUnari;
 
     @Override
     protected void onResume() {
@@ -22,6 +24,8 @@ public class CatProfileActivity extends AppCompatActivity {
         // 予め音声データを読み込む
         mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
         mSoundId = mSoundPool.load(getApplicationContext(), R.raw.cat1, 0);
+        mSoundIdKittin = mSoundPool.load(getApplicationContext(), R.raw.cat2, 0);
+        mSoundIdUnari = mSoundPool.load(getApplicationContext(), R.raw.cat2, 0);
     }
 
     @Override
@@ -29,12 +33,25 @@ public class CatProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_profile);
 
-        ((ImageView)findViewById(R.id.ivSpeaker)).setOnClickListener(new View.OnClickListener() {
+        ((ImageView)findViewById(R.id.ivSpeaker1)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSoundPool.play(mSoundId, 1.0F, 1.0F, 0, 0, 1.0F);
             }
         });
+        ((ImageView)findViewById(R.id.ivSpeaker2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mSoundIdKittin, 1.0F, 1.0F, 0, 0, 1.0F);
+            }
+        });
+        ((ImageView)findViewById(R.id.ivSpeaker3)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mSoundIdUnari, 1.0F, 1.0F, 0, 0, 1.0F);
+            }
+        });
+
     }
 
 }
